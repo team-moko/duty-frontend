aaaf019376bab39e9925ad8159e21dc403f27503
jsh3418 <albert5428@gmail.com>
2026-06-20 16:05:56 +0900

    feat: add vanilla-extract for CSS styling
    - Added @vanilla-extract/css and @vanilla-extract/next-plugin dependencies to package.json.
    - Created a new CSS file (page.css.ts) using vanilla-extract for styling.
    - Updated the Home component in page.tsx to apply the new styles.

---
+++ src/app/page.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const example = style({
+  minHeight: "100dvh",
+  display: "grid",
+  placeItems: "center",
+  fontSize: "2rem",
+  color: "red",
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import * as styles from "./page.css";
+
 export default function Home() {
-  return <div />;
+  return <div className={styles.example}>Hello</div>;
 }
