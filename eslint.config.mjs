7a3538153a356c6bc9b0db3c288910b56e1b69eb
jsh3418 <albert5428@gmail.com>
2026-06-21 21:13:28 +0900

    feat: add CI workflow and update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
